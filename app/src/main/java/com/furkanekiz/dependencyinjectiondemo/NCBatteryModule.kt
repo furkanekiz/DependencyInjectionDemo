d44174efe12b675fedc21041ec81ccaf528db7f2
furkanekiz <furkanekiz.06@gmail.com>
2022-08-22 17:30:16 +0300

    Working with Interfaces

---
+++ app/src/main/java/com/furkanekiz/dependencyinjectiondemo/NCBatteryModule.kt
@@ -0,0 +1,12 @@
+package com.furkanekiz.dependencyinjectiondemo
+
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+
+@Module
+abstract class NCBatteryModule {
+
+    @Binds
+    abstract fun bindsNCBattery(nickelCadmiumBattery: NickelCadmiumBattery): Battery
+}
